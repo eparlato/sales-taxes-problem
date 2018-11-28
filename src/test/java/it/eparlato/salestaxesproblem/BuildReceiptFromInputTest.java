a5c277f2b3e7d68115c146bfc861d1dc4a6d641c
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-11-28 11:24:16 +0100

    A receipt is built by a CashRegister when an input is provided.

---
+++ src/test/java/it/eparlato/salestaxesproblem/BuildReceiptFromInputTest.java
@@ -0,0 +1,20 @@
+package it.eparlato.salestaxesproblem;
+
+import org.junit.Test;
+import sun.security.util.SignatureFileVerifier;
+
+import static junit.framework.TestCase.assertNotNull;
+
+public class BuildReceiptFromInputTest {
+
+    @Test
+    public void a_receipt_should_be_built_if_an_input_is_provided() {
+        String input = "";
+
+        CashRegister cashRegister = new CashRegister();
+
+        Receipt receipt = cashRegister.process(input);
+
+        assertNotNull(receipt);
+    }
+}
---
+++ src/test/java/it/eparlato/salestaxesproblem/CashRegister.java
@@ -0,0 +1,7 @@
+package it.eparlato.salestaxesproblem;
+
+public class CashRegister {
+    public Receipt process(String input) {
+        return new Receipt();
+    }
+}
