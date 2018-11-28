551cdf9d116af8cced97aaa46d8621ec52ad961d
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-11-28 17:35:49 +0100

    We can build a Purchase object from an input string.

--- src/main/java/it/eparlato/salestaxesproblem/Purchase.java
+++ src/main/java/it/eparlato/salestaxesproblem/Purchase.java
@@ -24,4 +24,27 @@ public class Purchase {
     public BigDecimal getPrice() {
         return price;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Purchase)) {
+            return false;
+        }
+
+        Purchase that = (Purchase) obj;
+
+        if (this.quantity != that.quantity) {
+            return false;
+        }
+
+        if (!this.productName.equals(that.productName)) {
+            return false;
+        }
+
+        if (!(this.price.compareTo(that.price) == 0)) {
+            return false;
+        }
+
+        return true;
+    }
 }
---
+++ src/test/java/it/eparlato/salestaxesproblem/BuildingPurchaseTest.java
@@ -0,0 +1,20 @@
+package it.eparlato.salestaxesproblem;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BuildingPurchaseTest {
+    @Test
+    public void a_purchase_should_be_built_from_an_input() {
+        String input = "1 chocolate bar at 6.20";
+
+        Purchase expected = new Purchase(1, "chocolate bar", new BigDecimal(6.20));
+
+        PurchaseBuilder purchaseBuilder = new PurchaseBuilder();
+
+        assertEquals(expected, purchaseBuilder.buildFromInput(input));
+    }
+}
