1a9607ee5be7014c2cfe4a57349b5b3275f5971f
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-11-28 11:06:22 +0100

    We print a receipt with all values to zero if there are no purchases.

---
+++ src/test/java/it/eparlato/salestaxesproblem/BuildReceiptForSinglePurchaseTest.java
@@ -0,0 +1,20 @@
+package it.eparlato.salestaxesproblem;
+
+import org.junit.Test;
+
+import java.awt.print.PrinterJob;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BuildReceiptForSinglePurchaseTest {
+    @Test
+    public void empty_receipt_if_no_purchases_are_provided() {
+        String expected =
+                "Sales Taxes: 0.00\n" +
+                "Total: 0.00";
+
+        Receipt receipt = new Receipt();
+
+        assertEquals(expected, receipt.print());
+    }
+}
---
+++ src/test/java/it/eparlato/salestaxesproblem/Receipt.java
@@ -0,0 +1,10 @@
+package it.eparlato.salestaxesproblem;
+
+public class Receipt {
+
+    public String print() {
+        return
+            "Sales Taxes: 0.00\n" +
+            "Total: 0.00";
+    }
+}
