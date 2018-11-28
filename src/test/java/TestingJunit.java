e285fa8417429a18b6b2ed9f4604d7ca393d613a
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-11-28 10:24:51 +0100

    First commit, junit and maven correctly working.

---
+++ src/test/java/TestingJunit.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestingJunit {
+
+    @Test
+    public void junitWorks() {
+        assertTrue(true);
+    }
+}
