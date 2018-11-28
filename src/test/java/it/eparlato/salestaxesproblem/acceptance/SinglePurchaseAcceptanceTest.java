0b2c2ddeeead9671c3d29bfb72f6013fb6a903fc
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-11-28 12:30:01 +0100

    Acceptance test for the empty receipt case.

---
+++ src/test/java/it/eparlato/salestaxesproblem/acceptance/SinglePurchaseAcceptanceTest.java
@@ -0,0 +1,24 @@
+package it.eparlato.salestaxesproblem.acceptance;
+
+import it.eparlato.salestaxesproblem.CashRegister;
+import it.eparlato.salestaxesproblem.Receipt;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SinglePurchaseAcceptanceTest {
+    @Test
+    public void should_return_an_empty_receipt_if_the_input_is_empty() {
+        String input = "";
+
+        String expected =
+                "Sales Taxes: 0.00\n" +
+                "Total: 0.00";
+
+        CashRegister cashRegister = new CashRegister();
+
+        Receipt receipt = cashRegister.process(input);
+
+        assertEquals(expected, receipt.print());
+    }
+}
