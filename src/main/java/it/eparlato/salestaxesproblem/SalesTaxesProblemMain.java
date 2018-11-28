586e7ac93425c23cf2c5abc77d3daa819647fcf8
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-11-28 15:04:09 +0100

    Production classes moved into production package; created a main class to wire the components together.

---
+++ src/main/java/it/eparlato/salestaxesproblem/SalesTaxesProblemMain.java
@@ -0,0 +1,11 @@
+package it.eparlato.salestaxesproblem;
+
+public class SalesTaxesProblemMain {
+    public static void main(String[] args) {
+        CashRegister cashRegister = new CashRegister();
+
+        Receipt receipt = cashRegister.process("");
+
+        System.out.println(receipt.print());
+    }
+}
