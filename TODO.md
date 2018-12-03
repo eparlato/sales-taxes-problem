c560b804096dacc0fc268a380bc2938846749650
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2018-12-03 10:31:50 +0100

    A PurchaseBuilder can build a list of Purchase.

--- TODO.md
+++ TODO.md
@@ -4,3 +4,4 @@ Multiple purchases of one unit of tax free products
 Override hashCode in Purchase, refactor equals override
 Write tests for Purchase: is the regex expr valid for all the cases of the exercise?
 How do I properly parse a double from a string into a BigDecimal?
+Deal with plurals when building purchases? "1 book at...", "3 books at"
--- src/test/java/it/eparlato/salestaxesproblem/BuildingPurchaseTest.java
+++ src/test/java/it/eparlato/salestaxesproblem/BuildingPurchaseTest.java
@@ -2,7 +2,10 @@ package it.eparlato.salestaxesproblem;
 
 import org.junit.Test;
 
+import javax.swing.*;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -18,4 +21,22 @@ public class BuildingPurchaseTest {
         assertEquals(expected, purchaseBuilder.buildPurchasesFromInput(input).get(0));
     }
 
+    @Test
+    public void a_list_of_purchased_should_be_built_from_an_input() {
+        String input =
+                "3 books at 6.70\n" +
+                "4 chocolate bar at 4.10\n" +
+                "3 box of headache pills at 20.50";
+
+        List<Purchase> expectedPurchases =
+                Arrays.asList(
+                        new Purchase(3, "books", new BigDecimal(6.70)),
+                        new Purchase(4, "chocolate bar", new BigDecimal(4.10)),
+                        new Purchase(3, "box of headache pills", new BigDecimal(20.50))
+                        );
+
+        PurchaseBuilder purchaseBuilder = new RegexPurchaseBuilder();
+
+        assertEquals(expectedPurchases, purchaseBuilder.buildPurchasesFromInput(input));
+    }
 }
